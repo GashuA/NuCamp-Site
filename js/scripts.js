e1dc65a6e471226fe61f1f9c399face884279466
Gashaye <gashayem@gmail.com>
2022-02-05 11:02:13 -0500

    Reserve Campsites and Login Modal in JS

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,13 @@ $(function(){
       } 
   });
 });
+
+$(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('show')
+    })
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show')
+    })
+})
